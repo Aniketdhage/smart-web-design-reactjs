690545c733fb07b6d1973e77614091e2be20e1f0
Aniketdhage <aniketdhage90@gmail.com>
2023-02-15 13:57:44 +0530

    update : added container folder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import React from "react";
+import { Article, Brand, Navbar, Feature } from "./Components";
+import {
+  Blogs,
+  Footer,
+  Features,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./container";
 
 const App = () => {
   return <div>App</div>;
