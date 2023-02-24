26209646889afb6c4b94602e8329caafcf92e1cc
Aniketdhage <aniketdhage90@gmail.com>
2023-02-24 08:18:57 +0530

    update : added what gpt 3 component

--- src/Components/feature/Feature.jsx
+++ src/Components/feature/Feature.jsx
@@ -1,8 +1,33 @@
+// import React from "react";
+// import "./Feature.css";
+
+// const Feature = ({ title, text }) => {
+//   return (
+//     <div className="gpt3_features-container_feature">
+//       <div className="gpt3_features-container_feature-title">
+//         <div />
+//         <h1>{title}</h1>
+//       </div>
+//       <div className="gpt3_features-container_feature-text">{text}</div>
+//     </div>
+//   );
+// };
+
+// export default Feature;
+
 import React from "react";
 import "./Feature.css";
 
-const Feature = () => {
-  return <div>Feature</div>;
-};
+const Feature = ({ title, text }) => (
+  <div className="gpt3__features-container__feature">
+    <div className="gpt3__features-container__feature-title">
+      <div />
+      <h1>{title}</h1>
+    </div>
+    <div className="gpt3__features-container_feature-text">
+      <p>{text}</p>
+    </div>
+  </div>
+);
 
 export default Feature;
